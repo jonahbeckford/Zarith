94e44479204132bd106a6090f7e4f6dad03a8cc5
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-06-04 12:28:18 -0700

    Allow cross-compiling w/ dkml-c-probe

--- discover.ml
+++ discover.ml
@@ -1,8 +1,8 @@
 let () = Printexc.record_backtrace true
 
 let () =
-  if Array.length Sys.argv <> 2 then (
-    print_endline "Usage: ./configure_env.exe %{cc}";
+  if Array.length Sys.argv <> 4 then (
+    print_endline "Usage: ./configure_env.exe %{cc} %{ccomp_type} %{context_name}";
     exit 1)
 
 let uname () =
@@ -23,6 +23,8 @@ end = struct
 end
 
 let cc = Sys.argv.(1)
+let ccomp_type = Sys.argv.(2)
+let context_name = Sys.argv.(3)
 
 let ldflags =
   match Unix.getenv "LDFLAGS" with exception Not_found -> "" | s -> s
@@ -30,7 +32,7 @@ let ldflags =
 let cflags =
   match Unix.getenv "CFLAGS" with exception Not_found -> "" | s -> s
 
-let flags =
+let os_derived_flags =
   match Var.os with
   | "openbsd" | "freebsd" ->
       Printf.sprintf
@@ -47,5 +49,30 @@ let flags =
          -I/opt/local/include -I/usr/local/include\""
         ldflags cflags
   | _ -> ""
+
+let fmt_list_of_string_with_space fmt l =
+  List.iteri
+    (fun i s ->
+      if i > 0 then Format.fprintf fmt " ";
+      Format.fprintf fmt "%s" s)
+    l
+
+let flags =
+  let open Dkml_c_probe in
+  match C_conf.load_from_dune_context_name context_name with
+  | Error msg ->
+    Printf.eprintf "WARNING: [loading C_conf] %s\n" msg;
+    os_derived_flags
+  | Ok c_conf ->
+    match C_conf.compiler_flags_of_ccomp_type c_conf ~ccomp_type ~clibrary:"gmp" with
+    | Error msg ->
+      Printf.eprintf "WARNING: [compiler_flags] %s\n" msg;
+      os_derived_flags
+    | Ok None -> os_derived_flags
+    | Ok Some flags ->
+      Format.asprintf
+        "LDFLAGS=\"%a\" CFLAGS=\"%a\""
+        fmt_list_of_string_with_space (C_conf.C_flags.link_flags_pathonly flags)
+        fmt_list_of_string_with_space (C_conf.C_flags.cc_flags flags)
 
 let () = Printf.printf "CC=\"%s\" %s%!" cc flags
